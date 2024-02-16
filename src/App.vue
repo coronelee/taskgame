f2b306c6d56639c8eb6fd2cd99eb421097e524aa
coronelee <79922854+coronelee@users.noreply.github.com>
2024-02-16 20:01:31 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -70,28 +70,31 @@ const editTiming = (event) => {
 
 <template>
   <div class="bg-slate-800 w-screen h-screen flex justify-center items-center flex-col relative">
-    <h1 class="text-white font-sans text-5xl absolute top-10">Simon Says</h1>
+    <h1 class="text-white font-sans text-5xl  mt-10">Simon Says</h1>
     <div
-      class="flex items-center gap-20 max-[620px]:gap-5 max-[620px]:flex-col max-[620px]:h-3/4 max-[620px]:w-full p-5"
+      class="flex items-center gap-20 max-[745px]:gap-5 max-[745px]:flex-col max-[745px]:h-full max-[745px]:w-full p-5"
     >
       <div
-        class="rounded-full bg-white w-96 h-96 grid grid-cols-2 grid-rows-2 overflow-hidden shadow-cyan-500 shadow-md [&>*]:transition-[0.3s] max-[620px]:w-64 max-[620px]:h-64"
+        class="rounded-full bg-white w-96 h-96 grid grid-cols-2 grid-rows-2 overflow-hidden shadow-gray-500 shadow-2xl [&>*]:transition-[0.3s] max-[745px]:w-64 max-[745px]:h-64"
       >
-        <div class="bg-cyan-500 hover:brightness-200 cursor-pointer" @click="player" id="0"></div>
-        <div class="bg-red-500 hover:brightness-200 cursor-pointer" @click="player" id="1"></div>
-        <div class="bg-yellow-500 hover:brightness-200 cursor-pointer" @click="player" id="2"></div>
-        <div class="bg-lime-500 hover:brightness-200 cursor-pointer" @click="player" id="3"></div>
+        <div class="bg-gradient-to-t from-cyan-500 to-cyan-800 hover:brightness-200 cursor-pointer " @click="player" id="0"></div>
+        <div class="bg-gradient-to-t from-red-500 to-red-800 hover:brightness-200 cursor-pointer " @click="player" id="1"></div>
+        <div class="bg-gradient-to-b from-yellow-500 to-yellow-800 hover:brightness-200 cursor-pointer " @click="player" id="2"></div>
+        <div class="bg-gradient-to-b from-lime-500 to-lime-800 hover:brightness-200 cursor-pointer " @click="player" id="3"></div>
       </div>
       <div class="flex flex-col items-center">
-        <span class="text-white text-3xl font-sans font-bold">{{ countRounds }}</span>
-        <button
-          class="text-white mt-10 text-3xl font-sans font-bold bg-slate-700 px-4 py-2 rounded-xl"
+        <span class="text-white text-3xl font-sans font-bold">
+          Раунд: {{ countRounds }}</span>
+          <div class="flex gap-5 mt-5">
+
+            <button
+          class="text-white font-sans font-bold bg-slate-700 px-4 py-2 rounded-xl"
           @click="startGame"
         >
-          Play
+          Играть
         </button>
         <select
-          class="mt-10 font-sans font-bold bg-slate-700 px-4 py-2 rounded-xl text-white"
+          class=" font-sans font-bold bg-slate-700 px-4 py-2 rounded-xl text-white"
           defaultValue="1500"
           @click="editTiming($event)"
         >
@@ -99,6 +102,9 @@ const editTiming = (event) => {
           <option value="1000">Средний</option>
           <option value="400">Сложный</option>
         </select>
+
+          </div>
+        
       </div>
     </div>
   </div>
